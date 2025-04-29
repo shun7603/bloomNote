9bc9c0d5f23d7a40cfb240e72d80ec8d2dbc8c3d
shun7603 <summersao7603@icloud.com>
2025-04-29 21:17:09 +0900

    ユーザー管理機能
    マイグレーションファイル修正

--- db/migrate/20250428090258_create_care_relationships.rb
+++ db/migrate/20250428090258_create_care_relationships.rb
@@ -1,8 +1,8 @@
 class CreateCareRelationships < ActiveRecord::Migration[7.1]
   def change
     create_table :care_relationships do |t|
-      t.references :parent, null: false, foreign_key: {to_table: users}
-      t.references :caregiver, null: false, foreign_key: {to_table: users}
+      t.references :parent, null: false, foreign_key: {to_table: :users}
+      t.references :caregiver, null: false, foreign_key: {to_table: :users}
       t.references :child, null: false, foreign_key: true
       t.integer :status
 
