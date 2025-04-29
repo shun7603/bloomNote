af5ec1f00e18421c4a592be47616a28f973d786e
shun7603 <summersao7603@icloud.com>
2025-04-29 17:06:09 +0900

    ユーザー管理機能
    userモデルにenum追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { parent: 0, caregiver: 1 }
 end
