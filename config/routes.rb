f06e5211e71b2e548f77e11b3ffc8431a3c9466c
shun7603 <summersao7603@icloud.com>
2025-04-30 19:59:33 +0900

    子どもの登録
    home#index編集

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   resources :homes 
-  resources :children, only: [:new, :create, :index]
+  resources :children, only: [:new, :create, :index, :show]
 end
