912d9eead6a043524c56a2bb1391c16ab5f302a3
shun7603 <summersao7603@icloud.com>
2025-04-30 17:07:51 +0900

    子ども登録
    コントローラー作成

---
+++ app/controllers/children_controller.rb
@@ -0,0 +1,10 @@
+class ChildrenController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'children/new'
+  get 'children/index'
+  get 'children/create'
   root to: "homes#index"
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
