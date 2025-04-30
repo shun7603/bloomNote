3b916a4e4d1c8d95f525bc23355b908bfbb420a8
shun7603 <summersao7603@icloud.com>
2025-04-30 18:38:05 +0900

    子どもの登録
    show.htmlの追加

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -17,7 +17,26 @@ class ChildrenController < ApplicationController
   def index
     @children = current_user.children
   end
-
+  
+  def show
+    @child = current_user.children.find(params[:id])
+  
+    # 仮ルーティンを渡す
+    @routine = [
+      { time: "08:00", task: "ミルク" },
+      { time: "09:00", task: "睡眠" },
+      { time: "11:00", task: "排泄" }
+    ]
+  end
+  
+  def create
+    @child = current_user.children.build(child_params)
+    if @child.save
+      redirect_to child_path(@child), notice: '子どもを登録しました'
+    else
+      render :new
+    end
+  end
   private
 
   def child_params
