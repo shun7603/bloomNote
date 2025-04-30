61754e7fbe0c8ae7b6bd6acfd6dd2b98031f59c1
shun7603 <summersao7603@icloud.com>
2025-04-30 17:10:37 +0900

    子ども登録
    ビュー機能実装
    コントローラー実装
    ルーティング機能実装

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,10 +1,26 @@
 class ChildrenController < ApplicationController
-  def new
-  end
+  before_action :authenticate_user!
 
-  def index
+  def new
+    @child = Child.new
   end
 
   def create
+    @child = current_user.children.build(child_params)
+    if @child.save
+      redirect_to children_path, notice: '子どもを登録しました'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @children = current_user.children
+  end
+
+  private
+
+  def child_params
+    params.require(:child).permit(:name, :birth_date)
   end
 end
--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,6 @@
 class Child < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :birth_date, presence: true
 end
