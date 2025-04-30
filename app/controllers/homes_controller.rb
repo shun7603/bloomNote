a5ee8076d5bfda8e9b111ad555c5849a56de908e
shun7603 <summersao7603@icloud.com>
2025-04-30 16:45:35 +0900

    ホーム画面UI設計
    仮ビュー作成
    js関係作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,10 @@
+# app/controllers/homes_controller.rb
 class HomesController < ApplicationController
   def index
-    @records = Record.order(created_at: :desc).limit(10)
-  end
-
-  def add_milk
-    
+    @routine = [
+      { time: '08:00', task: 'ミルク' },
+      { time: '09:00', task: '睡眠' },
+      { time: '11:00', task: '排泄' }
+    ]
   end
 end
