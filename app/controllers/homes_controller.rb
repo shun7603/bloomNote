40f47295f47fa301de07f63faabd7ccd3f9be48f
shun7603 <summersao7603@icloud.com>
2025-04-30 15:13:37 +0900

    ホーム画面UI設計
    ルーティング設定

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,8 @@ class HomesController < ApplicationController
   def index
     @records = Record.order(created_at: :desc).limit(10)
   end
+
+  def add_milk
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: "homes#index"
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :homes 
+  
+
+
 end
