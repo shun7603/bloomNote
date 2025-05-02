56a165155a80bdf5e4f3a8c95a61addcdb60acb5
shun7603 <summersao7603@icloud.com>
2025-05-02 14:10:00 +0900

    親と預かる側の関係構築
    ハンバーガーラック作成
    「保育者を追加」のためのcreateアクション追加

---
+++ app/controllers/care_relationships_controller.rb
@@ -0,0 +1,55 @@
+class CareRelationshipsController < ApplicationController
+  before_action :set_children_and_users, only: [:new, :create, :index]
+
+  def index
+    @care_relationships = CareRelationship.all.includes(:parent, :caregiver, :child)
+  end
+
+  def new
+    @care_relationship = CareRelationship.new
+  end
+
+  def create
+    caregiver = User.find_by(email: params[:care_relationship][:caregiver_email])
+    child = Child.find(params[:care_relationship][:child_id])
+
+    if caregiver
+      care_relationship = CareRelationship.new(
+        parent_id: current_user.id,
+        caregiver_id: caregiver.id,
+        child_id: child.id,
+        status: 0 # 初期状態は「預かり中」など
+      )
+
+      if care_relationship.save
+        redirect_to root_path, notice: "保育者を追加しました"
+      else
+        flash[:error] = care_relationship.errors.full_messages
+        redirect_to root_path
+      end
+    else
+      flash[:error] = ["そのメールアドレスのユーザーは存在しません"]
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @care_relationship = CareRelationship.find(params[:id])
+    if @care_relationship.update(status: params[:status])
+      redirect_to care_relationships_path, notice: "ステータスを更新しました"
+    else
+      redirect_to care_relationships_path, alert: "ステータス更新に失敗しました"
+    end
+  end
+
+  private
+
+  def care_relationship_params
+    params.require(:care_relationship).permit(:parent_id, :caregiver_id, :child_id, :status)
+  end
+
+  def set_children_and_users
+    @children = Child.all
+    @users = User.all
+  end
+end
--- app/models/care_relationship.rb
+++ app/models/care_relationship.rb
@@ -1,5 +1,9 @@
 class CareRelationship < ApplicationRecord
-  belongs_to :parent
-  belongs_to :caregiver
+  belongs_to :parent, class_name: 'User'
+  belongs_to :caregiver, class_name: 'User'
   belongs_to :child
+
+  enum status: { active: 0, closed: 1 }
+
+  validates :parent_id, :caregiver_id, :child_id, :status, presence: true
 end
