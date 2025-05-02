b8bc98013a0d3837f085fe912de328d831f3a3da
shun7603 <summersao7603@icloud.com>
2025-05-02 12:06:02 +0900

    病院情報の登録
    エラーメッセージ編集

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -4,16 +4,16 @@ class Hospital < ApplicationRecord
 
   validates :name, presence: { message: '緊急連絡名を入力してください' }
   validates :phone_number, presence: { message: '電話番号を入力してください' }
-  validate :validate_phone_number_format
+  validate :phone_number_format_check
 
   private
 
-  def validate_phone_number_format
+  def phone_number_format_check
     return if phone_number.blank?
 
     if phone_number.include?('-')
-      errors.add(:phone_number, 'ハイフンなしの電話番号を入力してください')
-    elsif phone_number.length < 11 || phone_number =~ /\D/ || phone_number !~ /\A0\d{9,10}\z/
+      errors.add(:phone_number, 'ハイフンなしで入力してください')
+    elsif phone_number !~ /\A0\d{9,10}\z/
       errors.add(:phone_number, '有効な電話番号を入力してください')
     end
   end
