98d8450f04cfbc8dbbb5fa25fc9240a5dd453d2c
shun7603 <summersao7603@icloud.com>
2025-05-04 11:15:35 +0900

    ルーティン登録
    index追加
    ルーティン登録モーダル追加

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,7 +1,33 @@
 class RoutinesController < ApplicationController
-  belongs_to :child
+  before_action :set_child
+  def index
+    @children = current_user.children.includes(:routines)
+    @selected_date = params[:date]&.to_date || Date.current
+    @records = Record.where(recorded_at: @selected_date.all_day)
 
-  enum category: { milk: 0, sleep: 1, toilet: 2, meal: 3 }
+    @care_relationships = current_user.care_relationships.includes(:child, :parent, :caregiver)
+  end
 
-  validates :time, :task, :category, presence: true
+  def create
+    @routine = @child.routines.build(routine_params)
+    if @routine.save
+      flash[:notice] = "ルーティンを登録しました"
+      redirect_to root_path
+    else
+      flash[:alert] = "ルーティン登録に失敗しました"
+      flash[:routine_errors] = @routine.errors.full_messages
+      flash[:open_modal] = "routineModal"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def set_child
+    @child = Child.find(params[:child_id])
+  end
+
+  def routine_params
+    params.require(:routine).permit(:time, :task, :category, :memo, :photo)
+  end
 end
