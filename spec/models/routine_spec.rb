ebe099c6a5429ae1d5d149a1bc390ad753df1c5e
shun7603 <summersao7603@icloud.com>
2025-05-08 17:27:48 +0900

    RoutineモデルのRSpec単体テストとFactoryBotを追加
    ルーティンモデルの単体テストコード作成

--- spec/models/routine_spec.rb
+++ spec/models/routine_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Routine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:child) { FactoryBot.create(:child) }
+
+  subject do
+    described_class.new(
+      child: child,
+      time: "08:00",
+      task: "milk"
+    )
+  end
+
+  describe 'バリデーション' do
+    it '有効な内容で保存できる' do
+      expect(subject).to be_valid
+    end
+
+    it 'timeがないと無効' do
+      subject.time = nil
+      expect(subject).to be_invalid
+      expect(subject.errors[:time]).to include("を入力してください")
+    end
+
+    it 'taskがないと無効' do
+      subject.task = nil
+      expect(subject).to be_invalid
+      expect(subject.errors[:task]).to include("を入力してください")
+    end
+
+    it 'childが紐づいていないと無効' do
+      subject.child = nil
+      expect(subject).to be_invalid
+    end
+  end
+
+  describe '#task_label' do
+    it 'taskがmilkのとき、日本語表記を返す' do
+      subject.task = "milk"
+      expect(subject.task_label).to eq("ミルク")
+    end
+  end
+
+  describe '.task_options_for_select' do
+    it '日本語ラベルと英語キーの配列を返す' do
+      expect(Routine.task_options_for_select).to include(%W[\u30DF\u30EB\u30AF milk])
+    end
+  end
 end
